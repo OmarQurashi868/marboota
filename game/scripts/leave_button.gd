a5d92152cff26f9dc219df65a98e16f3c81522eb
Omar Qurashi <omarqurashi868@gmail.com>
2026-04-24 00:33:17 +0400

    fix loading

--- game/scripts/leave_button.gd
+++ game/scripts/leave_button.gd
@@ -8,7 +8,6 @@ func _ready() -> void:
 
 func _on_button_up() -> void:
 	Globals.sfx.play("ClickSfx")
-	Globals.sfx.play("SitSfx")
 	var player := Globals.my_player
 	var manager := Globals.player_manager
 	var seat: Seat = player.seat
